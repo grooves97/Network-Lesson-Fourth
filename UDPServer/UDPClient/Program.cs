d9531d54529796e4316b0def998038554a52d628
Аслан Нурсеитов <grooves97@gmail.com>
2019-06-21 12:05:52 +0600

    Простое приложение клиент-сервер консольного типа

---
+++ UDPServer/UDPClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace UDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Сокет для UDP обмена
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            EndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+
+            string str = "Hello world";
+            socket.SendTo(Encoding.UTF8.GetBytes(str), serverEndPoint);
+            str = "Bye world";
+            socket.SendTo(Encoding.UTF8.GetBytes(str), serverEndPoint);
+            str = "Madi lox";
+            socket.SendTo(Encoding.UTF8.GetBytes(str), serverEndPoint);
+            str = "CJ";
+            socket.SendTo(Encoding.UTF8.GetBytes(str), serverEndPoint);
+            str = "Travis Scott";
+            socket.SendTo(Encoding.UTF8.GetBytes(str), serverEndPoint);
+        }
+    }
+}
---
+++ UDPServer/UDPServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket serverUDP = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            string ipAddress = "0.0.0.0";
+            int port = 12345;
+
+            byte[] buf = new byte[64 * 1024];
+
+            //Локальная сетевая конечная точка(EdnPoint) для UDP сервера
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(ipAddress),port);
+            //Связывание адреса и порта с серверным UDP сокетом
+            serverUDP.Bind(serverEndPoint);
+            EndPoint remEndPoint = new IPEndPoint(0, 0);
+
+            Console.WriteLine("UDP сервер запущен");
+            while (true)
+            {
+                //ReceiveFrom() - метод приема данных по UDP протоколу
+                int recSize = serverUDP.ReceiveFrom(buf, ref remEndPoint);
+                Console.WriteLine("Полученные данные от клиента");
+                Console.WriteLine($"Size = {recSize}");
+                Console.WriteLine(Encoding.UTF8.GetString(buf,0,recSize));
+            }
+        }
+    }
+}
